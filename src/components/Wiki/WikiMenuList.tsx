731b0ecd880b3234c6a429820779af76430c3d4a
matsubarashoki <52858823+matsubarashoki@users.noreply.github.com>
2024-11-17 12:55:09 +0000

    update wikiList

--- src/components/Wiki/WikiMenuList.tsx
+++ src/components/Wiki/WikiMenuList.tsx
@@ -1,11 +1,12 @@
-import { Box, useTheme } from "@mui/material";
+import { Box, Button, Dialog, DialogActions, DialogContent, IconButton, Menu, MenuItem, useTheme } from "@mui/material";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { WikiRecords } from "../../utils/mock/Wiki";
 import { WikiType } from "../../utils/type/type";
+import { MoreVert } from "@mui/icons-material";
 
 interface WikiMenuListProps {
   setIsEditerOpen: Dispatch<SetStateAction<boolean>>;
@@ -14,11 +15,27 @@ interface WikiMenuListProps {
 
 const WikiMenuList = ({ setIsEditerOpen, setValue }: WikiMenuListProps) => {
   const theme = useTheme();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const [deleteDiqlogOpen, setDeleteDialogOpen] = useState<boolean>(false);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const handleListItemClick = (contents: string) => {
     setIsEditerOpen(true);
     setValue(contents);
   };
+
+  const handleWikiAdd = () => {
+    setIsEditerOpen(true)
+    setValue("")
+  }
 
   // 階層構造を作成
   function buildHierarchy(wikis: WikiType[]): WikiType[] {
@@ -63,10 +80,36 @@ const WikiMenuList = ({ setIsEditerOpen, setValue }: WikiMenuListProps) => {
               >
                 <ListItemText primary={node.title} />
               </ListItemButton>
+              <IconButton
+                aria-label="more"
+                aria-controls="long-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+              >
+                <MoreVert />
+              </IconButton>
             </ListItem>
             {node.children && node.children.length > 0 && (
               <List sx={{ paddingLeft: 2 }}>{renderTree(node.children)}</List>
             )}
+            <Menu
+              id="long-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              PaperProps={{
+                style: {
+                  maxHeight: 48 * 4.5, // メニューの高さを調整
+                  width: '20ch',
+                },
+              }}
+            >
+              <MenuItem onClick={handleWikiAdd}>追加</MenuItem>
+              <MenuItem onClick={() =>
+                handleListItemClick(node.contents)}>編集</MenuItem>
+              <MenuItem onClick={() => setDeleteDialogOpen(true)}>削除
+              </MenuItem>
+            </Menu>
           </div>
         );
       });
@@ -85,8 +128,15 @@ const WikiMenuList = ({ setIsEditerOpen, setValue }: WikiMenuListProps) => {
       role="presentation"
     >
       <List>{WikiTree(WikiConvertedRecords)}</List>
+      <Dialog open={deleteDiqlogOpen}>
+        <DialogContent>本当に削除しますか?</DialogContent>
+        <DialogActions>
+          <Button onClick={}>キャンセル</Button>
+          <Button>削除</Button></DialogActions>
+      </Dialog>
     </Box>
   );
 };
 
 export default WikiMenuList;
+
