3c442e2cdffe98775cdcf34ffc67ded77973bdd6
matsubarashoki <52858823+matsubarashoki@users.noreply.github.com>
2024-11-22 09:40:38 +0000

    update manageplan

--- src/components/ManagementPlan/ManagementPlanTop.tsx
+++ src/components/ManagementPlan/ManagementPlanTop.tsx
@@ -1,6 +1,14 @@
-import { Box, Container } from "@mui/material";
+import { Box, Card, Container, Grid2, Paper, styled } from "@mui/material";
+import NewsTable from "./NewsTable";
 
 const ManagementPlanTop = () => {
+
+  const StyledCard = styled(Card)(() => ({
+    width: "400px",
+    height: "200px",
+    margin:"8px",
+  }))
+
   return (
     <Container>
       <Box
@@ -10,16 +18,23 @@ const ManagementPlanTop = () => {
         flexGrow={1}
         minHeight={"calc(100vh - 200px)"}
       >
-        <div>経営計画</div>
-        <div>期間</div>
-        <div>経営理念</div>
-        <div>経営方針</div>
-        <div>経営戦略</div>
-        <div>アクションプラン</div>
-        <div>過去計画参照</div>
+        <Grid2 container>
+          <Box width={'100%'} display={'flex'} justifyContent={"space-between"} m={1} textAlign={'center'}>
+            <StyledCard>Term</StyledCard>
+            <StyledCard>一転語</StyledCard>
+          </Box>
+          <Grid2 >
+    
+              <NewsTable />
+
+  
+          </Grid2>
+        </Grid2>
       </Box>
     </Container>
   );
 };
 
 export default ManagementPlanTop;
+
+
